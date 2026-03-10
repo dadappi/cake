6f4b8138273b5713f40b82ff7897426e2f5efd7d
dadappi <dhafiibrohim1@gmail.com>
2026-03-10 16:11:06 +0700

    Update login1.js

--- login1.js
+++ login1.js
@@ -2,3 +2,7 @@ const container = document.querySelector(".container");
 container.addEventListener("animationend", () => {
   container.classList.remove("active");
 });
+function playMusic() {
+    var music = document.getElementById("music");
+    music.play();
+}
