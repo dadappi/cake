b7f30b47f49f0dbf45ce9b95e62155f0432310f9
dadappi <dhafiibrohim1@gmail.com>
2026-03-27 20:19:41 +0700

    Delete login1.js

--- login1.js
+++
@@ -1,8 +0,0 @@
-const container = document.querySelector(".container");
-container.addEventListener("animationend", () => {
-  container.classList.remove("active");
-});
-function playMusic() {
-    var music = document.getElementById("music");
-    music.play();
-}
